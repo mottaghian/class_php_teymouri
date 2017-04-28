aba794a484d22fd525cfb058309df35b894edeac
mottaghian <mehdi@mottaghian.com>
2017-04-28 19:44:06 +0430

    Add files via upload

---
+++ homework1.php
@@ -0,0 +1,26 @@
+<?php
+$txt1="Learn PHP";
+$txt2="Sematec PHP Class by Mr. Teymouri";
+$cars=array("Volvo","BMW","Toyota","Samand");
+
+echo $txt1;
+echo "<br>";
+echo "<h3>Study PHP at $txt2</h3>";
+echo "<h2>My car is a {$cars[3]}" , " but I like {$cars[0]}" , " .</h2>";
+echo '*************************************************';
+///////////////////////////////////////////////////////
+
+$firstName = array("Mehdi","Mehrdad","Amir","Alireza");
+$lastName = "Mottaghian";
+
+echo "<br>";
+echo "<h2>Your name is {$firstName[0]}" , " {$lastName}" , " .</h2>";
+echo "<h2>Your name is {$firstName[1]}" , " {$lastName}" , " .</h2>";
+echo "<h2>Your name is {$firstName[2]}" , " {$lastName}" , " .</h2>";
+echo "<h2>Your name is {$firstName[3]}" , " {$lastName}" , " .</h2>";
+echo '*************************************************';
+
+
+
+
+?>
